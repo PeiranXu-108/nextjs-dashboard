222683ab006e85a90b9dc31627ae2ab43d2990ee
dashuaige@berkeley.edu <xupeiran2048@gmail.com>
2025-06-21 21:23:00 +0800

    upt

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+// This file is used to keep the fonts that will be used in the application.
+import { Inter, Lusitana } from "next/font/google";
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
